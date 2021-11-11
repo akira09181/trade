ea7e47f0f3b55d4993ea33f83e5e20a8621ec92f
Akira <freelance0918@gmail.com>
2021-11-11 09:59:02 +0900

    fix

--- mytrade/urls.py
+++ mytrade/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('',views.index, name = 'index'),
     path('results',views.results,name='results'),
     path('sma',views.sma,name="sma"),
+    path('breverse',views.breverse,name="breverse"),
 ]
